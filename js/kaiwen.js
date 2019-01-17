e555f4ed60bf6fbe8083dc51d3c909637e23c23d
MEI93-PC\Administrator <kevinyeh3d@gmail.com>
2019-01-17 18:14:31 +0800

    修改

--- js/kaiwen.js
+++ js/kaiwen.js
@@ -7,22 +7,19 @@ $(function(){
 			autoClose: true
 		}, function() { //callback function
 		});
-		$('.btn01').on("click", function(){
+		$('.menu ul li:nth-of-type(1)').on("click", function(){
+			  	$('html,body').animate({scrollTop:$("#a00").offset().top},800);
+			 });
+		$('.menu ul li:nth-of-type(2)').on("click", function(){
 			  	$('html,body').animate({scrollTop:$("#a01").offset().top},800);
 			 });
-		$('.btn02').on("click", function(){
+		$('.menu ul li:nth-of-type(3)').on("click", function(){
 			  	$('html,body').animate({scrollTop:$("#a02").offset().top},800);
 			 });
-		$('.btn03').on("click", function(){
+		$('.menu ul li:nth-of-type(4)').on("click", function(){
 			  	$('html,body').animate({scrollTop:$("#a03").offset().top},800);
 			 });
-		$('.btn04').on("click", function(){
-			  	$('html,body').animate({scrollTop:$("#a04").offset().top},800);
-			 });
-		$('.btn05').on("click", function(){
-			  	$('html,body').animate({scrollTop:$("#a05").offset().top},800);
-			 });
-		$('.gotop').on("click", function(){
+		$('.menu ul li:nth-of-type(5)').on("click", function(){
 			  	$('html,body').animate({scrollTop:0},800);
 			 });
 
